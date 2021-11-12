af2e3d0a31aa5f0c496be080ac45f773733bd95b
Arthur Butler <arthurbutler1738@gmail.com>
2021-11-12 12:20:48 +0200

    first commit

---
+++ build/precache-manifest.e5d3029e5e5b7c841e0bbffd1b118823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d60b0248006671a5d7036aef129042e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa58c422794a68f58c9b",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "6d8d18dee740b63bf219",
+    "url": "/static/js/2.ab6cd6fa.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.ab6cd6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa58c422794a68f58c9b",
+    "url": "/static/js/main.939bf2a5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
